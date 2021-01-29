4d0c00097e69463098f98e1ce8af195407e73dad
bhanuprathap2000 <you@example.com>
2021-01-29 15:52:14 +0530

    included the build

---
+++ build/precache-manifest.cc5a46cb7096af46159037a24bc09af8.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2659f85f949c1da4dadf76d2019c8722",
+    "url": "/bhanuportfolio/index.html"
+  },
+  {
+    "revision": "b824429f57a92a7d7d90",
+    "url": "/bhanuportfolio/static/css/main.81accecf.chunk.css"
+  },
+  {
+    "revision": "e7c500a7217564371144",
+    "url": "/bhanuportfolio/static/js/2.e49888b6.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/bhanuportfolio/static/js/2.e49888b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b824429f57a92a7d7d90",
+    "url": "/bhanuportfolio/static/js/main.389a41fb.chunk.js"
+  },
+  {
+    "revision": "e636d7b3089cb0ac6b03",
+    "url": "/bhanuportfolio/static/js/runtime-main.22a5c079.js"
+  },
+  {
+    "revision": "12786bc79ed7b7c3753201fe4fe4f8ba",
+    "url": "/bhanuportfolio/static/media/algo.12786bc7.svg"
+  },
+  {
+    "revision": "406f6ef21ed7610dd94e28ec61eb65f7",
+    "url": "/bhanuportfolio/static/media/api.406f6ef2.svg"
+  },
+  {
+    "revision": "306ae54d171380112cb411e4bde3e136",
+    "url": "/bhanuportfolio/static/media/backend.306ae54d.svg"
+  },
+  {
+    "revision": "17444c3f774045b14e567a85b9cc2902",
+    "url": "/bhanuportfolio/static/media/bhanu Prathap.17444c3f.jpg"
+  },
+  {
+    "revision": "24bf224a9cb2ceb885c5e89e66505004",
+    "url": "/bhanuportfolio/static/media/circuit.24bf224a.png"
+  },
+  {
+    "revision": "16d20bbc975f5124a8799dd0616c9c7a",
+    "url": "/bhanuportfolio/static/media/computer.16d20bbc.svg"
+  },
+  {
+    "revision": "f9de737f18535765b99b5f08a6d54c2b",
+    "url": "/bhanuportfolio/static/media/facebook.f9de737f.svg"
+  },
+  {
+    "revision": "2c472603337cec187259ed6cd32b21e3",
+    "url": "/bhanuportfolio/static/media/github.2c472603.svg"
+  },
+  {
+    "revision": "99325d4efd59006419033a0523a5b0ba",
+    "url": "/bhanuportfolio/static/media/html5.99325d4e.svg"
+  },
+  {
+    "revision": "bb5903db7ccae981497f8565c90f801d",
+    "url": "/bhanuportfolio/static/media/instagram.bb5903db.svg"
+  },
+  {
+    "revision": "05dba3179852a52dbdb5f38fdbb763f1",
+    "url": "/bhanuportfolio/static/media/javascript.05dba317.svg"
+  },
+  {
+    "revision": "a13311a36cde2cfa79bff078a24103bf",
+    "url": "/bhanuportfolio/static/media/mightycoder.a13311a3.svg"
+  },
+  {
+    "revision": "9aa92c215ad62783f883c2067b42a719",
+    "url": "/bhanuportfolio/static/media/pin.9aa92c21.svg"
+  },
+  {
+    "revision": "cdfaf911081077c33647e3a43053c99f",
+    "url": "/bhanuportfolio/static/media/puzzle.cdfaf911.svg"
+  },
+  {
+    "revision": "e2905a883d5f95ef0907cc9adf27ea2d",
+    "url": "/bhanuportfolio/static/media/python.e2905a88.svg"
+  },
+  {
+    "revision": "9e188349967014b7f52086a714a73c49",
+    "url": "/bhanuportfolio/static/media/react.9e188349.svg"
+  },
+  {
+    "revision": "3725763121b8e64729ce8bb7f2cd0a74",
+    "url": "/bhanuportfolio/static/media/repair.37257631.svg"
+  },
+  {
+    "revision": "2942bfabb3d05332b66eb128e0842cff",
+    "url": "/bhanuportfolio/static/media/resume.2942bfab.pdf"
+  },
+  {
+    "revision": "d8c6f55311b71e7f30c8d4727f929bd7",
+    "url": "/bhanuportfolio/static/media/tie.d8c6f553.svg"
+  },
+  {
+    "revision": "714446c00041f89f1cab49b3eb0b4c6b",
+    "url": "/bhanuportfolio/static/media/urlshortener.714446c0.png"
+  },
+  {
+    "revision": "035a7336da3d4f1e0bd1934d2dfbe9fd",
+    "url": "/bhanuportfolio/static/media/weatherapp.035a7336.png"
+  }
+]);

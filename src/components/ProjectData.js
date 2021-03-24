16d4d107ee80459179fa9f21a44c7b7137d3767a
bhanuprathap2000 <you@example.com>
2021-03-24 09:25:36 +0530

    I have made change to github finder

--- src/components/ProjectData.js
+++ src/components/ProjectData.js
@@ -52,7 +52,7 @@ const data_projects = [
         image: githubfinder,
         deployed_url: 'https://githubuserprofilesearch.netlify.app/',
         github_url: 'https://github.com/bhanuprathap2000/githubusers',
-        category: ['React']
+        category: ['react.js']
     }
 ]
 

894df534d1944568c63ce973c6f503037cdd72d3
bhanu prathap <bhanuprathap2000@gmail.com>
2021-05-18 11:24:54 +0530

    Added 3 more projects 2 vanilla 1 django

--- src/components/ProjectData.js
+++ src/components/ProjectData.js
@@ -4,7 +4,9 @@ import CustomerBook from "../assets/projects_images/customerbook.png"
 import indexcal from "../assets/projects_images/indexcal.png"
 import randompassword from "../assets/projects_images/randompassword.png"
 import githubfinder from "../assets/projects_images/githubfinder.png"
-
+import quoteGenerator from '../assets/projects_images/quotegenerator.png'
+import infiniteScroll from '../assets/projects_images/infiniteScroll.png'
+import studentRegistration from '../assets/projects_images/studentRegistration.png'
 const data_projects = [
     
 
@@ -53,7 +55,31 @@ const data_projects = [
         deployed_url: 'https://githubuserprofilesearch.netlify.app/',
         github_url: 'https://github.com/bhanuprathap2000/githubusers',
         category: ['react.js']
-    }
+    },
+    {
+        name: 'Quote Generator',
+        image: quoteGenerator,
+        deployed_url: 'https://bhanuprathap2000.github.io/Quote-Generator/',
+        github_url: 'https://github.com/bhanuprathap2000/Quote-Generator',
+        category: ['vanilla']
+    },
+    {
+        name: 'Infinite Image Scroll',
+        image: infiniteScroll,
+        deployed_url: 'https://infinite-image-scroll.netlify.app/',
+        github_url: 'https://github.com/bhanuprathap2000/infinite-image-scroll',
+        category: ['vanilla']
+    },
+    {
+        name: 'Student Registartion',
+        image: studentRegistration,
+        deployed_url: 'https://github.com/bhanuprathap2000/student-registration',
+        github_url: 'https://github.com/bhanuprathap2000/student-registration',
+        category: ['python']
+    },
+
+
+    
 ]
 
 export default data_projects;
